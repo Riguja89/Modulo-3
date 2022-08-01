7c1cfe128473485053a0cfd271f32aada524d0d0
Riguja89 <jaimegutierrios@gmail.com>
2022-08-01 18:36:54 -0500

    resolve parte 2 del advance promise

--- 04-AdvancePromises/homework/src/pledge.js
+++ 04-AdvancePromises/homework/src/pledge.js
@@ -3,8 +3,74 @@
 Promises Workshop: construye la libreria de ES6 promises, pledge.js
 ----------------------------------------------------------------*/
 // // TU CÓDIGO AQUÍ:
+function $Promise(executor){
+    if(typeof executor!= 'function'){
+       throw new TypeError('The argument or executor  is not a function' );
+    }
 
+    this._state='pending';
+    this._handlerGroups=[];
 
+    this._internalResolve = (data)=>{
+       if(this._state=='pending'){
+        this._value=data;
+        this._state='fulfilled';
+
+        if(this._handlerGroups.length!=0){
+                this._handlerGroups.forEach((e)=>{ 
+                if(typeof e.successCb=='function') e.successCb(this._value)
+                if(typeof e.errorCb=='function') e.errorCb(this._value)
+        });
+        }
+
+       }
+        
+    };
+    this._internalReject= (rason)=>{
+        if(this._state=='pending'){
+        this._value=rason
+        this._state='rejected';
+        }
+        
+    };
+
+    const resolve= (data)=>{
+       this._internalResolve(data)
+    };
+    const reject=(rason)=>{
+        this._internalReject(rason)
+    };   
+
+    executor(resolve,reject);
+
+  
+    
+    this.then=function(s1,e1){
+        if(typeof s1 != 'function') s1=null;
+        if(typeof e1 != 'function') e1=null;
+        this._handlerGroups.push({successCb: s1,errorCb: e1})
+            
+        this._callHandlers(s1);
+    };
+
+   
+
+    this._callHandlers=function(handler){
+
+        if(this._state=='fulfilled'){
+        //     //this._callHandlers(s1);
+
+        if(typeof handler == 'function'){
+            handler(this._value)
+            //this._handlerGroups.shift()
+        }   
+     }  
+     //aca posiblemete haya que hacer algo cunado este en modo pending 
+    }
+   
+}
+
+const executor= new $Promise();
 
 module.exports = $Promise;
 /*-------------------------------------------------------
