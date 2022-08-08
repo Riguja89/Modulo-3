57e1b817af3b7d093988dc66e51c78c5bde1dc6e
Riguja89 <jaimegutierrios@gmail.com>
2022-08-07 22:17:51 -0500

    solucionado Testing

--- 06-Testing/homework/index.js
+++ 06-Testing/homework/index.js
@@ -11,8 +11,72 @@ app.get('/', (req, res) => {
 
 app.post('/product', (req, res) => {
   res.send({
-    result: req.body.a / req.body.b,
+    result: req.body.a * req.body.b,
   });
 });
+
+app.get('/test', (req, res) => {
+  res.send({
+    message: 'hola',
+  });
+});
+
+  app.post('/sum', (req, res) => {
+    res.send({
+      result: req.body.a + req.body.b,
+    });
+});
+
+app.post('/sumArray', (req, res) => {
+
+  let array= req.body.array;
+  let num = req.body.num;
+if (array.length>0){
+  for (i=0; i<array.length;i++){
+    for(j=i+1;j<array.length;j++){
+      if(array[i]+array[j]==num){
+        res.send({
+          result: true,
+        });
+      }
+    }
+  }
+}
+  res.send({
+    result: false,
+  });
+});
+
+app.post('/numString', (req, res) => {
+  let string= req.body.string;
+  if(typeof string!= 'number' && string.length>0){
+    res.status(200).send({
+      result: string.length,
+    });
+  }else{
+  res.status(400).send({
+    message: "Error en la solicitud"
+  });
+}
+});
+
+app.post('/pluck', (req, res) => {
+  let array= req.body.array;
+  let propiedad=req.body.propiedad;
+  let myarray=[]
+ 
+  if(array.length>0 && propiedad){
+    
+    array.map((o)=>myarray.push(o[propiedad]));
+    res.send({
+      result: myarray,
+    });
+  }else{
+  res.status(400).send({
+    message: "Error en la solicitud"
+  });
+}
+});
+
 
 module.exports = app; // Exportamos app para que supertest session la pueda ejecutar
--- 06-Testing/homework/tests/index.spec.js
+++ 06-Testing/homework/tests/index.spec.js
@@ -44,13 +44,63 @@ describe('Test de APIS', () => {
 
   describe('POST /sumArray', () => {
     it('responds with 200', () => agent.get('/test').expect(200));
-    it('responds with and object with message `test`', () =>
+    it('Debería devolver true si hay dos mumero con la posible suma ', () =>
       agent.post('/sumArray')
         .send({array: [2,5,7,10,11,15,20], num: 13})
         .then((res) => {
           expect(res.body.result).toEqual(true);
       }));
+    it('Debería devolver false si no hay dos mumero con la posible suma ', () =>
+      agent.post('/sumArray')
+        .send({array: [2,5,7,10,11,15,20], num: 14})
+        .then((res) => {
+          expect(res.body.result).toEqual(false);
+      }));  
   });
 
-});
+  describe('POST /numString', () => {
+    it('responds with 200', () => agent.get('/test').expect(200));
+    it('Debería devolver con 4 el string `hola`', () =>
+      agent.post('/numString')
+        .send({string: 'hola',})
+        .then((res) => {
+          expect(res.body.result).toEqual(4);
+      }));
+    it('Responder con un status 400 (bad request) si el string es un número.', () =>
+      agent.post('/numString')
+        .send({string: 3,})
+        .then((res)=>expect(res.status).toEqual(400)));
 
+    it('Responder con un status 400 (bad request) si el string esta vacio.', () =>
+      agent.post('/numString')
+        .send({string: '',})
+        .then((res)=>expect(res.status).toEqual(400)));
+
+  });
+
+        describe('POST /pluck', () => {
+          it('responds with 200', () => agent.get('/test').expect(200));
+          it('Responder con al funcionalidad del pluck.', () =>
+            agent.post('/pluck')
+              .send({array: [{color: 'azul', tamanio: 'grande', forma: 'cuadrado'},
+              {color: 'blanco', tamanio: 'mediano', forma: 'redondo'},
+              {color: 'verde', tamanio: 'pequeño', forma: 'triangular'}], propiedad: 'tamanio'})
+              .then((res) =>expect(res.body.result).toEqual(['grande', 'mediano','pequeño'])
+            ));
+
+            it('Responder con un status 400 (bad request) si array no es un arreglo.', () =>
+            agent.post('/pluck')
+              .send({array: 3,})
+              .then((res)=>expect(res.status).toEqual(400)));
+      
+          it('Responder con un status 400 (bad request) si el string propiedad está vacio.', () =>
+            agent.post('/pluck')
+              .send({array: [],})
+              .then((res)=>expect(res.status).toEqual(400)));
+
+          });
+        
+ 
+    
+
+});
